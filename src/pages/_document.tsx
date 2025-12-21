512b915c335d50ed9ad89b306adbefdb2795ebc0
franbenettini <franbenettini@gmail.com>
2025-12-21 19:14:31 -0300

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,14 @@ export default function Document() {
       <Head>
         <meta name="format-detection" content="telephone=no" />
         <meta name="msapplication-tap-highlight" content="no" />
+        {/* Favicon - configurado para tamaños más grandes */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="64x64" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" sizes="180x180" />
+        <meta name="msapplication-TileImage" content="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#9333EA" />
         {/* OneSignal SDK */}
         <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async />
       </Head>
